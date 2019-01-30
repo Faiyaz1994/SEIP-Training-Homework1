628eb5bc6228a5da7969841e7b8c908170587d57
Faiyaz1994 <faiyaz1994@gmail.com>
2019-01-30 23:03:48 +0600

    programs are added

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter Your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your Permanent Address:");
+            string permanentAddress = Console.ReadLine();
+            Console.WriteLine("Your Present Address:");
+            string presentAddress = Console.ReadLine();
+            Console.WriteLine("Your Mobile Number:");
+            string mobileNumber = Console.ReadLine();
+            Console.WriteLine("Your Date of Birth");
+            string dateOfBirth = Console.ReadLine();
+            Console.WriteLine("Your Age:");
+            string age = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Your Personal Information");
+            Console.WriteLine("===================================================");
+            Console.WriteLine("Name: "+name);
+            Console.WriteLine("Permanent Address: " +permanentAddress);
+            Console.WriteLine("Present Address: " +presentAddress);
+            Console.WriteLine("Mobile Number: " +mobileNumber);
+            Console.WriteLine("Date of Birth: " +dateOfBirth);
+            Console.WriteLine("Age: " +age);
+            Console.ReadKey();
+
         }
     }
 }
